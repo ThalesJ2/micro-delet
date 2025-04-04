7fd69af30d6f94a0c4d4848257b420810e022bc0
ThalesJ2 <thalesjorge36@gmail.com>
2025-04-03 21:50:06 -0300

    test

--- src/main/java/com/example/controllers/RestController.java
+++ src/main/java/com/example/controllers/RestController.java
@@ -3,7 +3,7 @@ package com.example.controllers;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 
-@Controller("/hello")
+@Controller()
 public class RestController {
 
     @Get("/")
